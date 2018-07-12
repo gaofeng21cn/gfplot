0c350a5f8e950eee00ab46ff7c08b443633579f1
gaofeng21cn <gaofeng21cn@hotmail.com>
2018-07-12 16:01:35 +0800

    add  plot_cor

--- R/plot_stat_figures.R
+++ R/plot_stat_figures.R
@@ -29,3 +29,42 @@ plot_barplot <- function() {
     scale_y_continuous(expand = c(0,0), limits = c(-1,1.25), trans = my_trans(from=-1)) + scale_fill_manual(values = c("#E69E00","#0070B0","#CA78A6", "#009C73")) +
     theme(legend.title = element_blank(), legend.position = "none", axis.title.x = element_blank()) + labs(y="m6A risk score")
 }
+
+
+
+#' Plot correlation
+#'
+#' This function plots correlation between two variables.
+#'
+#' @return a ggplot2 object of the plot
+#' @import ggplot2 cowplot
+#' @export
+#' @examples
+#'
+#'  library(cowplot)
+#'  plot_cor(iris$Sepal.Length, iris$Sepal.Width)
+
+plot_cor <- function(x, y, groups=NULL) {
+
+  pval <- paste(sprintf("Correlation = %.3f\nP", Hmisc::rcorr(x, y)$r[2,1]),
+             ifelse(Hmisc::rcorr(x, y)$P[2,1]==0, "< 1e-22",  paste0("= ", signif(Hmisc::rcorr(x, y)$P[2,1], 3)) ))
+
+  df.plot <- data.frame(a=x, b=y)
+
+  p <-   ggplot(df.plot, aes(x=a, y=b)) +
+    geom_smooth(method='lm',formula=y~x, se=F, linetype = "dashed", colour="grey50") +
+    theme(legend.title = element_blank(), legend.position = "none") +
+    annotate("text", x = Inf, y = Inf, hjust = 1, vjust = 1, label = pval)
+
+  if(!is.null(groups)) {
+    p <- p + geom_point(aes(color=groups), alpha=0.5)
+    if(length(unique(groups)) == 3) { p + scale_color_manual(values = c("#377EB8", "grey50", "#E41A1C")) }
+    if(length(unique(groups)) == 2) { p + scale_color_manual(values = c("#377EB8", "#E41A1C")) }
+    if(length(unique(groups)) > 3) { p + scale_color_brewer("Set1") }
+  } else {
+    p <- p + geom_point(alpha=0.5)
+  }
+  p
+}
+
+
